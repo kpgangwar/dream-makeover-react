f96be22344702e2799d3b8dc75848cab208f06f7
krishna pal <kpgangwar8909087801@gmail.com>
2026-01-10 22:36:59 +0530

    Perfected mobile hero spacing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,9 +21,65 @@
 
 // export default Hero;
 
+
+
+// import VoiceAssistant from "./VoiceAssistant";
+// import "../styles/hero.css";
+
+
+// const Hero = ({ openModal }) => {
+//   return (
+//     <>
+//       <VoiceAssistant />
+
+//       <section className="hero">
+//         <div className="hero-left">
+//           <h1>
+//             Dream <span>Makeover</span>
+//           </h1>
+
+//           <p>
+//             Bridal Makeup • Hair Styling • Mehndi • Facial • Nail Art <br />
+//             Keratin • Smoothening • Party & Wedding Packages
+//           </p>
+
+//           <button className="btn-book" onClick={openModal}>
+//             Book Your Glam Session
+//           </button>
+
+          {/* <div className="hero-buttons">
+  <button className="btn-book" onClick={openModal}>
+    Book Your Glam Session
+  </button>
+
+  <a
+    href="https://artificial-flower-house.netlify.app"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="btn-secondary"
+  >
+    Artificial Flower
+  </a>
+</div> */}
+
+
+//         </div>
+
+//         <div className="hero-right">
+//           <img src="/images/myphoto.jpg" alt="Makeup Model" />
+
+//         </div>
+//       </section>
+//     </>
+//   );
+// };
+
+// export default Hero;
+
+
+
 import VoiceAssistant from "./VoiceAssistant";
 import "../styles/hero.css";
-
 
 const Hero = ({ openModal }) => {
   return (
@@ -44,28 +100,10 @@ const Hero = ({ openModal }) => {
           <button className="btn-book" onClick={openModal}>
             Book Your Glam Session
           </button>
-
-          {/* <div className="hero-buttons">
-  <button className="btn-book" onClick={openModal}>
-    Book Your Glam Session
-  </button>
-
-  <a
-    href="https://artificial-flower-house.netlify.app"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="btn-secondary"
-  >
-    Artificial Flower
-  </a>
-</div> */}
-
-
         </div>
 
         <div className="hero-right">
           <img src="/images/myphoto.jpg" alt="Makeup Model" />
-
         </div>
       </section>
     </>
@@ -73,3 +111,4 @@ const Hero = ({ openModal }) => {
 };
 
 export default Hero;
+
