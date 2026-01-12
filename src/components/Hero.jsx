c733b451f92239f15f2cc3d108feb54005d95b18
krishna pal <kpgangwar8909087801@gmail.com>
2026-01-12 12:21:17 +0530

    update dream makeover

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,81 +1,3 @@
-// import VoiceAssistant from "./VoiceAssistant";
-// import '../styles/hero.css';
-
-// const Hero = ({ openModal }) => {
-//   return (
-//     <>
-//       <VoiceAssistant />
-
-//       <section className="hero">
-//         <h1>✨ Beautify Your Confidence ✨</h1>
-//         <p>Professional Makeup • Hair Styling • Mehndi • Facial • Nail Art <br />
-//          Keratin • Smoothening • Bridal Packages Available 👑</p>
-
-//         <button className="btn-book" onClick={openModal}>
-//           Book Appointment
-//         </button>
-//       </section>
-//     </>
-//   );
-// };
-
-// export default Hero;
-
-
-
-// import VoiceAssistant from "./VoiceAssistant";
-// import "../styles/hero.css";
-
-
-// const Hero = ({ openModal }) => {
-//   return (
-//     <>
-//       <VoiceAssistant />
-
-//       <section className="hero">
-//         <div className="hero-left">
-//           <h1>
-//             Dream <span>Makeover</span>
-//           </h1>
-
-//           <p>
-//             Bridal Makeup • Hair Styling • Mehndi • Facial • Nail Art <br />
-//             Keratin • Smoothening • Party & Wedding Packages
-//           </p>
-
-//           <button className="btn-book" onClick={openModal}>
-//             Book Your Glam Session
-//           </button>
-
-          {/* <div className="hero-buttons">
-  <button className="btn-book" onClick={openModal}>
-    Book Your Glam Session
-  </button>
-
-  <a
-    href="https://artificial-flower-house.netlify.app"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="btn-secondary"
-  >
-    Artificial Flower
-  </a>
-</div> */}
-
-
-//         </div>
-
-//         <div className="hero-right">
-//           <img src="/images/myphoto.jpg" alt="Makeup Model" />
-
-//         </div>
-//       </section>
-//     </>
-//   );
-// };
-
-// export default Hero;
-
 
 
 import VoiceAssistant from "./VoiceAssistant";
@@ -102,13 +24,12 @@ const Hero = ({ openModal }) => {
           </button>
         </div>
 
-        <div className="hero-right">
-          <img src="/images/myphoto.jpg" alt="Makeup Model" />
-        </div>
-      </section>
-    </>
+        <div className="hero-right">        
+             <img src="/images/myphoto.jpg" alt="Makeup Model" />
+        </div>       </section>     </>
   );
 };
 
 export default Hero;
+
 
