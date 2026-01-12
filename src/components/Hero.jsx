e00831e46c8b434b802063afe0c7c643ae5c0f5c
krishna pal <kpgangwar8909087801@gmail.com>
2026-01-12 15:21:40 +0530

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,68 +35,6 @@
 // export default Hero;
 
 
-
-
-// import { useEffect, useState } from "react";
-// import VoiceAssistant from "./VoiceAssistant";
-// import "../styles/hero.css";
-
-// const banners = [
-//   "/images/banner/makeup1.jpg",
-//   "/images/banner/makeup2.jpg",
-//   "/images/banner/makeup3.jpg",
-//   "/images/banner/makeup4.jpg",
-// ];
-
-// const Hero = ({ openModal }) => {
-//   const [index, setIndex] = useState(0);
-
-//   useEffect(() => {
-//     const timer = setInterval(() => {
-//       setIndex((prev) => (prev + 1) % banners.length);
-//     }, 3000);
-//     return () => clearInterval(timer);
-//   }, []);
-
-//   return (
-//     <>
-//       <VoiceAssistant />
-
-//       {/* 🔥 Sliding Banner Strip */}
-//       <div className="top-banner">
-//         <img src={banners[index]} alt="Dream Makeover Banner" />
-//       </div>
-
-//       {/* 💎 Your Hero stays unchanged */}
-//       <section className="hero">
-//         <div className="hero-left">
-//           <h1>
-//             Dream <span>Makeover</span>
-//           </h1>
-
-//           <p>
-//             Bridal Makeup • Hair Styling • Mehndi • Facial • Nail Art <br />
-//             Keratin • Smoothening • Party & Wedding Packages
-//           </p>
-
-//           <button className="btn-book" onClick={openModal}>
-//             Book Your Glam Session
-//           </button>
-//         </div>
-
-//         <div className="hero-right">
-//           <img src="/images/myphoto.jpg" alt="Makeup Model" />
-//         </div>
-//       </section>
-//     </>
-//   );
-// };
-
-// export default Hero;
-
-
-
-
 import { useEffect, useState } from "react";
 import VoiceAssistant from "./VoiceAssistant";
 import "../styles/hero.css";
@@ -108,7 +46,7 @@ const banners = [
   "/images/banner/makeup4.jpg",
 ];
 
-const Hero = ({ openModal }) => {
+function Hero({ openModal }) {
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
@@ -122,12 +60,10 @@ const Hero = ({ openModal }) => {
     <>
       <VoiceAssistant />
 
-      {/* Top Sliding Banner */}
       <div className="top-banner">
-        <img key={index} src={banners[index]} alt="Dream Makeover Banner" />
+        <img src={banners[index]} alt="Dream Makeover Banner" />
       </div>
 
-      {/* Hero */}
       <section className="hero">
         <div className="hero-left">
           <h1>
@@ -150,6 +86,6 @@ const Hero = ({ openModal }) => {
       </section>
     </>
   );
-};
+}
 
 export default Hero;
