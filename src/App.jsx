15833519820e64af5d03de1abec5f1df3e31dc87
krishna pal <kpgangwar8909087801@gmail.com>
2026-02-20 18:36:32 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,28 @@ import Footer from "./components/footer";
 
 import "./styles/global.css";
 
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+function PageTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (window.dataLayer) {
+      window.dataLayer.push({
+        event: "page_view",
+        page_path: location.pathname,
+      });
+    }
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter>
+     <PageTracker />
       <Routes>
 
         {/* Login – NO Navbar */}
